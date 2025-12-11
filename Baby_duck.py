5878bbed8dae21ec1f8426ca742c1c37edec0bcc
DavidDing <108644237+WithorwithoutU@users.noreply.github.com>
2025-12-11 13:18:28 -0500

    Final version of Baby_duck.py

--- Baby_duck.py
+++ Baby_duck.py
@@ -18,7 +18,7 @@ BUZZER = 16  # buzzer on BCM16, short leg to GND
 
 UDP_IP = ""
 UDP_PORT = 5005
-LEADER_IP = "10.49.243.186"
+LEADER_IP = "10.49.245.19" # might change frequently
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(TRIG_A, GPIO.OUT)
@@ -56,13 +56,13 @@ def get_distance(TRIG, ECHO):
     time.sleep(0.00001)
     GPIO.output(TRIG, 0)
 
-    timeout = time.time() + 0.02
+    timeout = time.time() + 0.03
     while GPIO.input(ECHO) == 0:
         if time.time() > timeout:
             return None
     start = time.time()
 
-    timeout = time.time() + 0.02
+    timeout = time.time() + 0.03
     while GPIO.input(ECHO) == 1:
         if time.time() > timeout:
             return None
@@ -74,16 +74,16 @@ def get_distance(TRIG, ECHO):
 
 
 def get_two_distances():
-    d_b = get_distance(TRIG_B, ECHO_B)
-    time.sleep(0.02)
     d_a = get_distance(TRIG_A, ECHO_A)
-
+    time.sleep(0.1)
+    d_b = get_distance(TRIG_B, ECHO_B)
+    
     return d_a, d_b
 
 
 def distance_to_speed(dist_cm):
     global lost_flag
-    if dist_cm is None or dist_cm <= 7:
+    if dist_cm is None or dist_cm <= 5:
         return 0
     elif dist_cm >= 80:
         # lost due to far distance
@@ -118,9 +118,31 @@ def set_motor_speeds(speed_a, speed_b):
     pwm_a.ChangeDutyCycle(spd_a)
     pwm_b.ChangeDutyCycle(spd_b)
 
-# NEW: params for alignment
-ALIGN_THRESHOLD = 2.0    # cm difference to start turning
-ALIGN_BOOST     = 20.0   # extra speed for the farther side
+
+ALIGN_THRESHOLD = 2.0
+ALIGN_THRESHOLD_PRO = 4.0
+ALIGN_BOOST = 15
+
+
+def fmt_dist(d):
+    if d is None:
+        return "None"
+    return f"{d:.1f}"
+
+
+def send_command(message):
+    sock.sendto(message.encode(), (LEADER_IP, UDP_PORT))
+    print("Send: ", message)
+
+
+def receive_command():
+    try:
+        data, addr = sock.recvfrom(1024)
+        print("Received: ", data)
+        return data.decode()
+    except socket.timeout:
+        return None
+
 
 try:
     while stop_flag:
@@ -139,17 +161,25 @@ try:
         base_speed = distance_to_speed(front_dist)
         speed_a = base_speed
         speed_b = base_speed
-
+        
         if dist_a is not None and dist_b is not None:
             diff = dist_a - dist_b
-
-            if diff > ALIGN_THRESHOLD:
+            
+            if diff > ALIGN_THRESHOLD and diff < ALIGN_THRESHOLD_PRO:
                 speed_a = min(base_speed + ALIGN_BOOST, 100)
                 speed_b = base_speed
+                
+            elif diff > ALIGN_THRESHOLD_PRO: 
+                speed_a = min(base_speed + ALIGN_BOOST + 10, 100)
+                speed_b = base_speed
 
-            elif diff < -ALIGN_THRESHOLD:
+            elif diff < -ALIGN_THRESHOLD and diff > ALIGN_THRESHOLD_PRO:
                 speed_b = min(base_speed + ALIGN_BOOST, 100)
                 speed_a = base_speed
+                
+            elif diff < -ALIGN_THRESHOLD_PRO:
+                speed_b = min(base_speed + ALIGN_BOOST + 10, 100)
+                speed_a = base_speed                
 
         set_motor_speeds(speed_a, speed_b)
 
@@ -184,7 +214,7 @@ try:
             f"SpeedA:{speed_a:.0f}% SpeedB :{speed_b:.0f}%"
         )
         
-        time.sleep(0.2)
+        time.sleep(0.01)
 
 except KeyboardInterrupt:
     print("stop")
